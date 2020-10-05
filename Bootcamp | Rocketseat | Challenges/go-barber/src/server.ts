d951ea2d986a81b30b21a62e820f2397020a8c3f
Thiago Andrade Silva <thiago.adsix@gmail.com>
2020-10-05 20:13:39 -0300

    :construction: Accessing avatar with route

--- Bootcamp | Rocketseat | Challenges/go-barber/src/server.ts
+++ Bootcamp | Rocketseat | Challenges/go-barber/src/server.ts
@@ -1,12 +1,15 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes/index';
-import 'reflect-metadata';
+import uploadConfig from './config/upload';
 
 import './database/index';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
