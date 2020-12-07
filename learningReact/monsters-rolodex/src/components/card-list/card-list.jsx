80f0221b299b5c2c7a21120f217c7e17d82c435b
Thiago Andrade Silva <thiago.adsix@gmail.com>
2020-12-07 07:33:18 -0300

    :construction: Some codes about Typescript and ReactJS

---
+++ learningReact/monsters-rolodex/src/components/card-list/card-list.jsx
@@ -0,0 +1,14 @@
+import { Card } from "../card/card";
+import "./card-list.css";
+
+export const CardList = (props) => {
+  console.log(props);
+
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
